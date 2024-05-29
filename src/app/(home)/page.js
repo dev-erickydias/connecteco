e5ac0cb696ad6c6fb30f09d014502a3e53d4e4de
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-29 13:15:00 +0100

    add bg-color in pages add banner

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,6 +1,7 @@
 import { ButtonsMaterials } from "@/components/buttonMaterials/ButtonsMaterials";
-import { Banner } from "./components/Banner";
+
 import { CardsList } from "./components/CardsList";
+import { Banner } from "./components/banner/Banner";
 
 export default function Home() {
   return (
