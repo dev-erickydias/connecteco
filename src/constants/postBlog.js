9663ad0976753d166471ce5dd09b130bcdff2b42
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-30 10:49:11 +0100

    add esquema de blog

--- src/constants/postBlog.js
+++ src/constants/postBlog.js
@@ -1,30 +1,30 @@
 function gerarIdUnico() {
-    return 'id-' + Math.random().toString(36).substr(2, 9);
-  }
-  
-  // Array de objetos postBlog
-  const postBlog = [
-    {
-      id: gerarIdUnico(),
-      titulo: "Aprendendo JavaScript",
-      descricao: "Uma introdução ao JavaScript para iniciantes.",
-      imagem: "https://exemplo.com/imagens/javascript.png",
-      link: "https://exemplo.com/aprendendo-javascript"
-    },
-    {
-      id: gerarIdUnico(),
-      titulo: "CSS para Iniciantes",
-      descricao: "Guia básico para começar a usar CSS em seus projetos.",
-      imagem: "https://exemplo.com/imagens/css.png",
-      link: "https://exemplo.com/css-para-iniciantes"
-    },
-    {
-      id: gerarIdUnico(),
-      titulo: "Dominando o HTML",
-      descricao: "Tudo o que você precisa saber sobre HTML.",
-      imagem: "https://exemplo.com/imagens/html.png",
-      link: "https://exemplo.com/dominando-html"
-    }
-  ];
-   
-  export default postBlog
+  return "id-" + Math.random().toString(36).substr(2, 9);
+}
+
+// Array de objetos postBlog
+const postBlog = [
+  {
+    id: gerarIdUnico(),
+    titulo: "Aprendendo JavaScript",
+    descricao: "Uma introdução ao JavaScript para iniciantes.",
+    imagem: "https://exemplo.com/imagens/javascript.png",
+    link: "https://exemplo.com/aprendendo-javascript",
+  },
+  {
+    id: gerarIdUnico(),
+    titulo: "CSS para Iniciantes",
+    descricao: "Guia básico para começar a usar CSS em seus projetos.",
+    imagem: "https://exemplo.com/imagens/css.png",
+    link: "https://exemplo.com/css-para-iniciantes",
+  },
+  {
+    id: gerarIdUnico(),
+    titulo: "Dominando o HTML",
+    descricao: "Tudo o que você precisa saber sobre HTML.",
+    imagem: "https://exemplo.com/imagens/html.png",
+    link: "https://exemplo.com/dominando-html",
+  },
+];
+
+export default postBlog;
