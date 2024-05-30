58528a6ccc4e65a6a73a7bf271e543cfb25f7bdc
Ericky Dias <erickyhenriquesd@gmail.com>
2024-05-30 19:02:11 +0100

    correção de conflitos

--- src/constants/bannerInfo.js
+++ src/constants/bannerInfo.js
@@ -4,15 +4,8 @@ function gerarIdUnico() {
 
 const bannerInfo = [
   {
-<<<<<<< HEAD
-    texts: {
-      title: "Encontre pontos de reciclagem perto de você!",
-      description: "Para um futuro sustentável. Veja onde reciclar!!"
-    },
-=======
     title: "Encontre pontos de reciclagem perto de você!",
     description: "Para um futuro sustentável. Veja onde reciclar!",
->>>>>>> feature/blog-page
     id: gerarIdUnico(),
     src: "/BannerOne.png",
     alt: "Peneus para reciclar",
@@ -21,15 +14,8 @@ const bannerInfo = [
     className: "banner__image_object-contain",
   },
   {
-<<<<<<< HEAD
-    texts: {
-      title: "Encontre pontos de reciclagem perto de você!",
-      description: "Para um futuro sustentável. Veja onde reciclar!"
-    },
-=======
     title: "Nosso Blog",
     description: "Fique por dentro de todas as novidades e informações sobre reciclagem e sustentabilidade.",
->>>>>>> feature/blog-page
     id: gerarIdUnico(),
     src: "/BannerOne.png",
     alt: "Peneus para reciclar",
@@ -38,15 +24,8 @@ const bannerInfo = [
     className: "banner__image_object-contain",
   },
   {
-<<<<<<< HEAD
-    texts: {
-      title: "Encontre pontos de reciclagem perto de você!",
-      description: "Para um futuro sustentável. Veja onde reciclar!"
-    },
-=======
     title: "Faça parte",
     description: "Preencha o formulário abaixo e tenha seu ponto de coleta em destaque.",
->>>>>>> feature/blog-page
     id: gerarIdUnico(),
     src: "/BannerOne.png",
     alt: "Peneus para reciclar",
