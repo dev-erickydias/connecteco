e32a6eb4249537215eab2c587295cad7a8cbee8f
maikoncorrea <maikonacorrea@gmail.com>
2024-05-30 01:30:04 -0300

    adjustments to the ButtonsMaterials.jsx and css component

--- src/components/ButtonMaterials/ButtonsMaterials.jsx
+++ src/components/ButtonMaterials/ButtonsMaterials.jsx
@@ -1,35 +1,54 @@
 "use client";
 import "./buttonsMaterials.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import CustomButton from "@/components/CustomButton.jsx";
+import materials from "@/constants/materials.js";
 
 export function ButtonsMaterials() {
-  const materials = [
-    "Todos",
-    "Plásticos",
-    "Papel",
-    "Vidro",
-    "Eletrônicos",
-    "Metais",
-    "Medicamentos",
-    "Textil",
-  ];
-
   const [selectedMaterial, setSelectedMaterial] = useState(materials[0]);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
 
   const handleChange = (event) => {
     setSelectedMaterial(event.target.value);
   };
 
+  useEffect(() => {
+    function handleResize() {
+      setIsLargeScreen(window.innerWidth > 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="materials">
       <p className="materials__title">Qual tipo de material você gostaria de descartar?</p>
-      <select className="materials__select" value={selectedMaterial} onChange={handleChange}>
-        {materials.map((material, index) => (
-          <option className="materials__option" key={index} value={material}>
-            {material}
-          </option>
-        ))}
-      </select>
+      {isLargeScreen ? (
+        <div className="materials__grid">
+          {materials.map((material, index) => (
+            <CustomButton
+              key={index}
+              className={`materials__button ${selectedMaterial === material ? 'materials__button--selected' : ''}`}
+              onClick={() => setSelectedMaterial(material)}
+            >
+              {material}
+            </CustomButton>
+          ))}
+        </div>
+      ) : (          
+          <select className="materials__select" value={selectedMaterial} onChange={handleChange}>
+            {materials.map((material, index) => (
+              <option className="materials__option" key={index} value={material}>
+                {material}
+              </option>
+            ))}
+          </select>
+      )}
     </div>
   );
 }
+
