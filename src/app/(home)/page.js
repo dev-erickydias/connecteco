6063ea6277d9998bcefb0a97b350c3e2ff783ba3
maikoncorrea <maikonacorrea@gmail.com>
2024-05-31 11:14:37 -0300

    card creation and search in the Cards.jsx component and css config

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,6 +1,6 @@
 import { ButtonsMaterials } from "@/components/buttonMaterials/ButtonsMaterials";
 
-import { CardsList } from "./components/CardsList";
+import { CardsList } from "../../components/cards/CardsList";
 import { Banner } from "./components/banner/Banner";
 
 import bannerInfo from "@/constants/bannerInfo";
