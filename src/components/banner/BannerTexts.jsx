aa1fd2e8d55be4f39a48b654a7e95e3b3ea0edbf
Ericky Dias <erickyhenriquesd@gmail.com>
2024-06-01 11:13:10 +0100

    add responsividade do banner e add refatoração do componente adicionando button

--- src/components/banner/BannerTexts.jsx
+++ src/components/banner/BannerTexts.jsx
@@ -1,8 +1,10 @@
-export default function BannerTexts({ title, description }) {
+import CustomButton from "../CustomButton";
+export default function BannerTexts({ title, description, btn }) {
   return (
     <div className="banner__texts">
       <h2 className="banner__texts_title">{title}</h2>
       <h3 className="banner__texts_description">{description}</h3>
+      {btn?<CustomButton className={"banner__btn"}>Faça parte</CustomButton>: null}
     </div>
   );
 }
